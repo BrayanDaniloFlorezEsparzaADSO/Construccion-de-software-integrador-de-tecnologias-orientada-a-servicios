0996d303786d05e178281424cd603a8a6226c0e6
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2022-11-19 22:33:37 -0500

    seccion 1 punto 6

--- seccion 1/main.js
+++ seccion 1/main.js
@@ -1,22 +1,22 @@
 addEventListener("DOMContentLoaded", (e)=>{
     let form=document.querySelector('#input')
+
     let cont =1   
     form.addEventListener("submit",(e)=>{
         e.preventDefault();       
 
         if(cont <=50){
         let datosEntrada = Object.fromEntries(new FormData(e.target))
-            let observaciones= datosEntrada.observaciones
-            let hora= (datosEntrada.hora)
 
+            let observaciones= datosEntrada.observaciones
             let tabla=document.querySelector("tbody");
             tabla.insertAdjacentHTML("beforeend",`
                 <tr>
                     <td> ${cont}  </td>
                     <td> ${observaciones}  </td>
-                    <td> ${hora} </td>
                 </tr>
-                `) 
+                `)
+
             cont++
         }
     })               
