ae235c470dc3a4f67c737cd6c2e2658eb86e11cd
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2022-11-19 22:26:16 -0500

    seccion 1 punto 5

--- seccion 1/main.js
+++ seccion 1/main.js
@@ -1,42 +1,23 @@
 addEventListener("DOMContentLoaded", (e)=>{
     let form=document.querySelector('#input')
-
+    let cont =1   
     form.addEventListener("submit",(e)=>{
         e.preventDefault();       
 
+        if(cont <=50){
         let datosEntrada = Object.fromEntries(new FormData(e.target))
+            let observaciones= datosEntrada.observaciones
+            let hora= (datosEntrada.hora)
 
-            let nciudad= datosEntrada.ciudad
-            let f= Number(datosEntrada.Fahrenheit)
-            let res=(f-32) / 1.8
             let tabla=document.querySelector("tbody");
             tabla.insertAdjacentHTML("beforeend",`
                 <tr>
-
-                    <td> ${nciudad}  </td>
-                    <td> ${f}° </td>
-                    <td> ${res}° </td>
+                    <td> ${cont}  </td>
+                    <td> ${observaciones}  </td>
+                    <td> ${hora} </td>
                 </tr>
-                `)          
-    })     
-    let form1=document.querySelector('#input2')
-
-    form1.addEventListener("submit",(e)=>{
-        e.preventDefault();       
-
-        let datosEntrada = Object.fromEntries(new FormData(e.target))
-
-            let nciudad2= datosEntrada.ciudad2
-            let f1= Number(datosEntrada.CENTIGRADOS)
-            let res2=(f1* 1.8 )+ 32
-            let tabla=document.querySelector("tbody[name=tbody]");
-            tabla.insertAdjacentHTML("beforeend",`
-                <tr>
-
-                    <td> ${nciudad2}  </td>
-                    <td> ${f1} °</td>
-                    <td> ${res2} °</td>
-                </tr>
-                `)          
-    })        
+                `) 
+            cont++
+        }
+    })               
 })
