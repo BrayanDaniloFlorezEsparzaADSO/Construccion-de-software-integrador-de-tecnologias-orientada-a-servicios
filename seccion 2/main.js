1a800f280136a6c3f50d01c0eca8662b0aa294c2
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2022-11-29 21:45:49 -0500

    seccion 2 punto 4

--- seccion 2/main.js
+++ seccion 2/main.js
@@ -7,21 +7,15 @@ addEventListener("DOMContentLoaded", (e)=>{
 
         if(cont <=50){
         let datosEntrada = Object.fromEntries(new FormData(e.target))
-        let distancia=Number(42.195)
-        let tiempo= Number(datosEntrada.tiempo)
-            let res= (distancia/tiempo)
-            let res2=   (res*0.001)
-            let res4=res2.toFixed(2)
-            let res3=res.toFixed(2)
+        let C= Number(datosEntrada.centigrados)
+        let res= (C * 9/5) + 32 
         let tabla=document.querySelector("tbody")
         tabla.insertAdjacentHTML("beforeend",`
             <tr>
             
-                <td> ${cont}  </td>
-                <td> ${distancia} KM </td>
-                <td> ${tiempo} H </td>
-                <td> ${res3}  M </td>
-                <td> ${res4}  KM </td>
+                <td> ${C}  °</td>
+            <td> ${res} °</td>
+
             </tr>
             `)
         cont++
