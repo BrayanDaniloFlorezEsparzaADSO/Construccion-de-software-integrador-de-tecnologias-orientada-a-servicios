d5f4148a0923037c20ec3625c65146de870d0346
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2022-11-29 22:22:21 -0500

    seccion 2 punto 5

--- seccion 2/main.js
+++ seccion 2/main.js
@@ -7,17 +7,55 @@ addEventListener("DOMContentLoaded", (e)=>{
 
         if(cont <=50){
         let datosEntrada = Object.fromEntries(new FormData(e.target))
-        let C= Number(datosEntrada.centigrados)
-        let res= (C * 9/5) + 32 
-        let tabla=document.querySelector("tbody")
+        let taller1= Number(datosEntrada.taller_1)
+        let taller2= Number(datosEntrada.taller_2)
+        let quiz=Number(datosEntrada.quiz)
+        let parcial=Number(datosEntrada.parcial)
+
+        let res= taller1+taller2
+        let res2= (res *15)/100
+
+        let res3=(quiz*15)/100
+
+        let res4=(parcial*70)/100
+
+        let res5= res2+res3+res4
+        if(res5>=70){
+          let tabla=document.querySelector("tbody")
         tabla.insertAdjacentHTML("beforeend",`
             <tr>
             
-                <td> ${C}  °</td>
-            <td> ${res} °</td>
+                <td> ${cont}</td>
+                <td> ${taller1}</td>
+                <td> ${taller2}</td>
+                <td> ${quiz}</td>
+                <td> ${parcial}</td>
+                <td> ${res5}</td>
+                <td> aprobado</td>
+                
+
+            </tr>
+            `)  
+        }
+        else{
+            let tabla=document.querySelector("tbody")
+        tabla.insertAdjacentHTML("beforeend",`
+            <tr>
+            
+                <td> ${cont}</td>
+                <td> ${taller1}</td>
+                <td> ${taller2}</td>
+                <td> ${quiz}</td>
+                <td> ${parcial}</td>
+                <td> ${res5}</td>
+                <td> reprobado</td>
+                
 
             </tr>
             `)
+        }
+
+        
         cont++
         }
     })               
