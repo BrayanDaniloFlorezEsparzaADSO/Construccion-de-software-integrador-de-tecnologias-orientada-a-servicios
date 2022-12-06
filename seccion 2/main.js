9906cb468259247aa0426281f6ff7b079c8bec87
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2022-12-06 11:24:05 -0500

    seccion 2 punto 9

--- seccion 2/main.js
+++ seccion 2/main.js
@@ -1,38 +1,58 @@
 addEventListener("DOMContentLoaded", (e)=>{
     let form=document.querySelector('#hola')
     let conta= 1
+    let suma=0
     form.addEventListener("submit",(e)=>{
         e.preventDefault();   
         if(conta<=20){
             let datosEntrada = Object.fromEntries(new FormData(e.target))  
-            let producto= datosEntrada.producto
+            let cliente= datosEntrada.cliente
+            let valor=Number(datosEntrada.valor)
+            let des=50000
+            
+            if(valor>=des){
+                let res= valor*0.20
+                let res2=valor-res
+                // let suma1=res2
+                let tabla=document.querySelector("tbody")
+                tabla.insertAdjacentHTML("beforeend",`
+                    
+                <tr>
+                    <td > ${conta}</td>
+                    <td > ${cliente}</td>
+                    <td > ${valor}</td>
+                    <td > X</td>
+                    <td > </td>
+                    <td >${res2} </td>
+                   
+                </tr> 
+                    `)     
+                    let total = document.querySelector("#hola2")  
+                    total.innerHTML = " "
+                total.insertAdjacentHTML("beforeend", `${suma+=res2}`)  
+            }   
+            
+            else{
+                // let suma= valor 
+                let tabla=document.querySelector("tbody")
+                tabla.insertAdjacentHTML("beforeend",`
+                    
+                <tr>
+                    <td > ${conta}</td>
+                    <td > ${cliente}</td>
+                    <td > ${valor}</td>
+                    <td > </td>
+                    <td > X</td>
+                    <td >${valor} </td>
+                </tr> 
+                    `) 
+                    let total = document.querySelector("#hola2")  
+                    total.innerHTML = " "
+                    total.insertAdjacentHTML("beforeend", `${suma+=valor}`) 
+            }
 
-            let precio=Number (datosEntrada.precio)
-            let precio2= Number(datosEntrada.precio2)
-            let precio3= Number(datosEntrada.precio3)
-            let precio4= Number(datosEntrada.precio4)
-            let precio5= Number(datosEntrada.precio5)
-            let res= (precio+precio2+precio3+precio4+precio5)
-            let res2= (res*4803.77)
-            let res3= res2.toFixed(2)
-            let tabla=document.querySelector("tbody")
-            tabla.insertAdjacentHTML("beforeend",`
-                
-            <tr>
-                
-                <td > ${conta}</td>
-                <td > ${producto}</td>
-                <td > USD${res}</td>
-                <td> $${res3}</td>
-                
-            </tr> 
-                `)  
-                        
-
-                        
         conta++
         
         }
-        
     })               
 })
